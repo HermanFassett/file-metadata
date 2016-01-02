64521aa4ed64f7d572866a9526f116d18e87780b
HermanFassett <hermanfassett@gmail.com>
2016-01-01 17:08:41 -0800

    Add body parser

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -12,16 +16,8 @@ app.use(function(req, res, next) {
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname+"/index.html"));
 });
-app.get('/:timestamp', function(req, res) {
-  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  var timestamp = req.params.timestamp;
-  var result = { unix: null, natural: null };
-  var date = new Date(isNaN(timestamp) ? timestamp : parseInt(timestamp, 10)*1000);
-  if (date.getTime() > 0) {
-    result.unix = Math.floor(date.getTime() / 1000);
-    result.natural = months[date.getUTCMonth()] + " " + date.getUTCDate() + ", " + date.getUTCFullYear();
-  }
-  res.json(result);
+app.post('/analyze', function(req, res) {
+  res.json({"hi": "bye"});
 });
 
 app.listen(process.env.PORT || 3000);
