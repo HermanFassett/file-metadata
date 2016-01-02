ce7164d0b06fb54e1eb0d252509fb3468e39098f
HermanFassett <hermanfassett@gmail.com>
2016-01-01 22:40:03 -0800

    Add uploading

--- server.js
+++ server.js
@@ -1,24 +1,34 @@
 var http = require('http');
 var path = require('path');
 var express = require('express');
+var multer = require('multer');
+var fs = require("fs");
+var upload = multer({ dest: 'uploads/'});
 var app = express();
-
-var bodyParser = require('body-parser')
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
+// Set access control
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
+// Default index
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname+"/index.html"));
 });
-
-app.post('/analyze', function(req, res) {
-  res.json({"filename": req.body.file, "data": JSON.parse(req.body.filedata)});
+// Analyze url
+app.post('/analyze', upload.single("file"), function(req, res) {
+  // Get uploaded file data
+  var file = {
+    "name": req.file.originalname,
+    "type": req.file.mimetype,
+    "size": req.file.size
+  }
+  // Delete file
+  fs.unlink(req.file.path, function(err) {
+    if (err) return console.error(err);
+    console.log("File deleted successfully!");
+  });
+  // Send data
+  res.json(file);
 });
-
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000); // Listen on port
